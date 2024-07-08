9010caaa6b699e38de07f409e24d07bae6bbbdaf
tuPC <phatdtgcs220340@fpt.edu.vn>
2024-07-08 14:19:24 +0700

    Add project files.

---
+++ models/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationCentre.models
+{
+    public abstract class Person
+    {
+        public Person(int id, Profile profile) { 
+            this.Id = id;
+            this.Profile = profile;
+        }
+        public int Id { get; }
+        public Profile Profile { get; }
+    }
+    public class Student : Person
+    {
+        public Student(int id, Profile profile) : base(id, profile)
+        {
+            this.Profile.Role = Role.Student;
+        }
+    }
+    public class TeachingStaff : Person
+    {
+        public TeachingStaff(int id, Profile profile) : base(id, profile)
+        {
+            this.Profile.Role = Role.TeachingStaff;
+        }
+    }
+    public class Administration : Person
+    {
+        public Administration(int id, Profile profile) : base(id, profile)
+        {
+            this.Profile.Role = Role.Administration;
+        }
+    }
+}
---
+++ models/Profile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationCentre.models
+{
+    public enum Role
+    {
+        Student,
+        TeachingStaff,
+        Administration
+    }
+    public class Profile
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Telephone { get; set; }
+        public string Email {  get; set; }
+        public Role Role {  get; set; }
+    }
+}
